0a04df56f32dd410d1470dc20a95315e5c59ac0a
Aerraerr <marquezaeronjead@gmail.com>
2025-03-08 20:59:02 +0800

    Added login modal and improved responsiveness

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+Route::get('/login', function () {
+    return view('Registration.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('Registration.register');
+})->name('register');
